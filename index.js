c763de9febfb3d4c4c5421c96b46271a06ab8c6b
Maxim Mazurok <maxim@mazurok.com>
2024-07-23 13:51:21 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+// start vanilla nodejs server:
+
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+  if (req.url.toLowerCase().endsWith(".html")) {
+    fs.readFile(path.join(__dirname, req.url), "utf8", (err, data) => {
+      res.writeHead(200, {
+        "Content-Type": "text/html",
+      });
+      res.write(data);
+      res.end();
+    });
+  } else if (req.url.toLowerCase().endsWith(".css")) {
+    fs.readFile(path.join(__dirname, req.url), "utf8", (err, data) => {
+      res.writeHead(200, {
+        "Content-Type": "text/css",
+      });
+      res.write(data);
+      res.end();
+    });
+  } else {
+    res.writeHead(404, {
+      "Content-Type": "text/html",
+    });
+    res.write("<h1>Page Not Found</h1>");
+    res.end();
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Server is running, visit http://localhost:3000");
+});
